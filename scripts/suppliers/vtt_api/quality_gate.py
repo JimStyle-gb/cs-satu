27e4e7b0cf126f4e058159a7a9bd32429a591f63
JimStyle-gb <JimStyle.88@mail.ru>
2026-04-08 17:53:24 +0500

    Create quality_gate.py

---
+++ scripts/suppliers/vtt_api/quality_gate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Path: scripts/suppliers/vtt_api/quality_gate.py
+Тонкая обёртка над текущим VTT quality gate.
+"""
+from suppliers.vtt.quality_gate import run_quality_gate  # noqa: F401
